210867a35b999b581dac3ba5ce3abc7b4477da88
mathias <mathiauni2007@gmail.com>
2025-09-09 15:37:21 -0500

    EJERCICIO 5 SEMANA 3

--- SEMANA-1_C#/Program.cs
+++ SEMANA-1_C#/Program.cs
@@ -10,7 +10,7 @@ namespace SEMANA_1_C_
     {
         static void Main(string[] args)
         {
-            ejer2();
+            ejer5();
             Console.ReadKey();
 
         }
@@ -47,10 +47,31 @@ namespace SEMANA_1_C_
 
         }
         static void ejer4()
-        { 
+        {
+            Console.Write("INGRESE UN NUMERO DECIMAL: ");
+            double num= Convert.ToDouble(Console.ReadLine());
+
+            double raiz2 = Math.Sqrt(num);
+            int redo = (int)Math.Round(num,0);
+            double cubo = Math.Pow(num,3); 
+            double raiz3 = Math.Pow(num,1/3d);
+
+            Console.WriteLine("RAIZ 2: " + raiz2);
+            Console.WriteLine("REDONDEADO: " + redo);
+            Console.WriteLine("AL CUBO: " + cubo);
+            Console.WriteLine("RAIZ 3: " + raiz3);
         }
         static void ejer5()
         {
+            Console.WriteLine("INGRESE NÚMERO: ");
+            string num = Console.ReadLine();
+
+            int entero = int.Parse(num);
+            double deci = double.Parse(num);
+
+            Console.WriteLine("RESTA: " + (entero % 2));
+            Console.WriteLine("DIVISION; " + (deci / 3));
+
 
         }
         static void ejer6()
--- SEMANA_!_PYTHON/SEMANA___PYTHON.py
+++ SEMANA_!_PYTHON/SEMANA___PYTHON.py
@@ -17,4 +17,23 @@ def ejer3():
     print("RESTA: ", (x-y))
     print("MULTIPLICACION: ", (x*y))
     print("DIVISION: ", (x/y))
-ejer2()
+
+
+import math #IMPORTANTE LA LIBRERIA MATH
+def ejer4():
+    num = float(input("INGRESE UN NUMERO DECIMAL: "))
+
+    print("RAIZ 2: ", math.sqrt(num))
+    print("REDONDEADO: " , round(num,0))
+    print("AL CUBO: ", math.pow(num,3))
+    print("RAIZ 3: ", num ** (1/3))
+
+def ejer5():
+    num = input("INGRESE NUMERO: ")
+
+    entero = int(num)
+    deci = float(num)
+
+    print("RESTA: ", (entero%2))
+    print("DIVISION: ", (deci/3)) 
+ejer5()
