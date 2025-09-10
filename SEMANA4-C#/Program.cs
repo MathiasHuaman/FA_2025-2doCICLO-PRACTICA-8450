5551c33835ccb0ff17111582f0d8259d6776c18b
Mathias <mathiauni2007@gmail.com>
2025-09-09 20:40:30 -0500

    EJERCICIOS_1Y2_SEMANA4

---
+++ SEMANA4-C#/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA4_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+        static void ejer1()
+        {
+            Console.Write("INGRESA UNA EDAD: ");
+            int edad=int.Parse(Console.ReadLine());
+            
+            if (edad < 18)
+            {
+                Console.WriteLine("Eres menor de edad");
+            }
+            else if (edad >= 18 && edad < 64)
+            {
+                Console.WriteLine("Eres mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Eres de la tercera edad");
+            }
+
+        }
+        static void ejer2()
+        {
+            Console.Write("INGRESA UN AÑO: ");
+            int año = int.Parse(Console.ReadLine());
+            bool bisiento;
+            if ((año % 4 == 0 && año % 100 != 0) || (año % 400 == 0))
+            {
+                bisiento = true;
+                Console.WriteLine($"El año {año} es bisiesto");
+            }
+            else
+            {
+                bisiento = false;
+                Console.WriteLine($"El año {año} no es bisiesto");
+            }
+            bool par = año % 2 == 0;
+
+            if (par)
+            {
+                Console.WriteLine($"El año {año} es par");
+            }
+            else
+            {
+                Console.WriteLine($"El año {año} es impar");
+            }
+
+        }
+        static void ejer3()
+        { 
+        }
+        static void ejer4()
+        { 
+        }
+    }
+}
--- SEMANA4-PY/SEMANA4_PY.py
+++ SEMANA4-PY/SEMANA4_PY.py
@@ -1,4 +1,4 @@
-def ejer1():
+﻿def ejer1():
     edad = int(input("INGRESE UNA EDAD: "))
     
     if edad < 18:
@@ -9,5 +9,26 @@ def ejer1():
         else:
             print("ADULTO MAYOR")
 
+def ejer2():
 
-ejer1()
+    año= int(input("INGRESE UN AÑO: "))
+
+    if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+        es_bisiento = True
+    else:
+        es_bisiento = False
+
+    es_par= año % 2 == 0
+
+    if es_bisiento:
+       print(f"El año {año} es bisiesto")
+    else:
+       print(f"El año {año} no es bisiesto")
+    if es_par:
+       print(f"El año {año} es par")
+    else:
+       print(f"El año {año} es impar")
+
+
+
+ejer2()
