e1f43598825f7ae8f4498991ba1bbda17b19dd7e
Mathias <mathiauni2007@gmail.com>
2025-09-16 15:33:42 -0500

    actualizacion_semana4

--- SEMANA4-PY/SEMANA4_PY.py
+++ SEMANA4-PY/SEMANA4_PY.py
@@ -29,6 +29,49 @@ def ejer2():
     else:
        print(f"El año {año} es impar")
 
+def ejer3():
+     monto = float(input("INGRESE EL MONTO EN SOLES: "))
+
+     print("\n1. DOLARES\n2. EUROS")
+
+     opcion = int(input("\nIngrese una opcion: "))
+
+     match opcion:
+         case 1: print("DOLARES: " ,(monto/3.75))
+         case 2: print("EUROS: " ,(monto/4.05))  
+         case _: print("OPCION INCORRECTA")
+
+import math
+
+def ejer4():
+    print("BIENVENIDO AL SISTEMA DE CALCULOS DE AREAS\n")
+    print("1. Cuadrado")
+    print("2. Rectangulo")
+    print("3. Triangulo")
+    print("4. Circulo\n")
+
+    opcion = int(input("INGRESE UNA OPCION: "))
+
+    match opcion:
+        case 1: 
+            lado=int(input("INGRESE LADO: "))
+            print("ÁREA: ", lado*lado)
+        case 2:
+            bse=int(input("INGRESE LA BASE: "))
+            alt=int(input("INGRESE LA ALTURA: "))
+            print("ÁREA RECTANGULO: " , (bse*alt))
+        case 3:
+             bse2=int(input("INGRESE LA BASE: "))
+             alt2=int(input("INGRESE LA ALTURA: "))
+             print("ÁREA TRIÁNGULO: " , (bse2*alt2)/2)
+        case 4:
+            radio=int(input("INGRESA EL RADIO: "))
+            print("ÁREA DEL CIRCULO: ", (round(math.pi * radio**2),2))
+
+        case _: print("OPCIÓN INCORRECTA")
 
 
-ejer2()
+
+
+
+ejer4()
